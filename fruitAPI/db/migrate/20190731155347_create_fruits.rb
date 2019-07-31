f17cacc04a9db6660c35dda403832c299970788a
Julian De Angelis <julian.deangelis@gmail.com>
2019-07-31 11:54:53 -0400

    Create Category and Fruit models

---
+++ fruitAPI/db/migrate/20190731155347_create_fruits.rb
@@ -0,0 +1,10 @@
+class CreateFruits < ActiveRecord::Migration[5.2]
+  def change
+    create_table :fruits do |t|
+      t.int :category_id
+      t.text :name
+
+      t.timestamps
+    end
+  end
+end
