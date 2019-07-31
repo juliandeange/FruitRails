a9ca97ee499dd49f99881000c90f079516135c0a
Julian De Angelis <julian.deangelis@gmail.com>
2019-07-31 16:01:08 -0400

    Create read operations for fruit
    available through:
    /fruit/{id}
    /category/{id}/fruit/{id}

--- fruitAPI/app/controllers/fruit_controller.rb
+++ fruitAPI/app/controllers/fruit_controller.rb
@@ -1,8 +1,21 @@
 class FruitController < ApplicationController
 
-    def index 
-        fruits = Fruit.all
-        render json: {status: "SUCCESS", message: "Loaded Fruits", data:fruits}, status: :ok
+    def index
+        # fruits = Fruit.all.where(:name => ["strawberry", "blackberry"])
+        fruits = Fruit.all.where(:category_id => params[:category_id])
+        render json: {status: "SUCCESS", message: "Loaded fruits...", data:fruits}, status: :ok
+    end
+
+    def show 
+        if params[:category_id]
+            fruit = Fruit.where(:category_id => params[:category_id], :id => params[:id])
+        else
+            fruit = Fruit.find(params[:id])
+        end
+        render json: {status: "SUCCESS", message: "Loaded fruit...", data:fruit}, status: :ok
+    end
+
+    def create
     end
 
 end
--- fruitAPI/config/routes.rb
+++ fruitAPI/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    resources :category
-    resources :fruit
+    resources :category do
+        resources :fruit, only: [:show, :index, :create]
+    end
+    resources :fruit, only: [:show]
 
 end
