6254fa63d00913501dc3626ea410904375e9d284
Julian De Angelis <julian.deangelis@gmail.com>
2019-07-31 15:03:12 -0400

    Create CRUD operation for category controller

--- fruitAPI/app/controllers/category_controller.rb
+++ fruitAPI/app/controllers/category_controller.rb
@@ -1,2 +1,47 @@
 class CategoryController < ApplicationController
+
+    ## CRUD
+
+    #Read
+    def index 
+        categories = Category.all
+        render json: {status: "SUCCESS", message: "Categories loaded...", data:categories},status: :ok
+    end
+
+    def show
+        category = Category.find(params[:id])
+        render json: {status: "SUCCESS", message: "Category loaded...", data:category},status: :ok
+    end
+
+    #Create
+    def create
+        category = Category.new(category_params)
+        if category.save
+            render json: {status: "SUCCESS", message: "Category created...", data:category},status: :ok
+        else
+            render json: {status: "ERROR", message: "Category no created...", data:category},status: :unprocessable_entity
+        end
+    end
+
+    #Delete
+    def destroy
+        Category.find(params[:id]).destroy
+        render json: {status: "SUCCESS", message: "Category deleted..."},status: :ok
+    end
+
+    #Update
+    def update
+        category = Category.find(params[:id])
+        if category.update_attributes(category_params)
+            render json: {status: 'SUCCESS', message: 'Category updated...', data:category}, status: :ok
+        else
+            render json: {status: 'ERROR', message: 'Category not updated...', data:category}, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def category_params
+        params.permit(:Fruit_Type)
+    end
+
 end
--- fruitAPI/app/controllers/fruit_controller.rb
+++ fruitAPI/app/controllers/fruit_controller.rb
@@ -1,2 +1,8 @@
 class FruitController < ApplicationController
+
+    def index 
+        fruits = Fruit.all
+        render json: {status: "SUCCESS", message: "Loaded Fruits", data:fruits}, status: :ok
+    end
+
 end
